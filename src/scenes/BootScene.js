6e60ec623c4173f2be437a591c27d641107bea14
wnstn4896 <wnstn4896@gmail.com>
2025-09-21 01:19:23 +0900

    modified:   assets/images/junsusuki_bullet.png 	new file:   assets/images/stone.png 	new file:   assets/images/stone_bullet.png 	new file:   assets/images/stone_bullet_eyes.png 	modified:   src/scenes/BootScene.js 	renamed:    src/scenes/ShooterScene.js -> src/scenes/Stage1BattleScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -11,6 +11,10 @@ export class BootScene extends Phaser.Scene {
         this.load.image('jaemin', `${basePath}/assets/images/jaemin.png`);
         this.load.image('junsusuki', `${basePath}/assets/images/junsusuki.png`);
 
+        this.load.image('stone', `${basePath}/assets/images/stone.png`);
+        this.load.image('stone_bullets', `${basePath}/assets/images/stone_bullets.png`);
+        this.load.image('stone_bullets_eyes', `${basePath}/assets/images/stone_bullets_eyes.png`);
+
         this.load.image('bullet', `${basePath}/assets/images/bullet.png`);
         this.load.image('junsusuki_bullet', `${basePath}/assets/images/junsusuki_bullet.png`);
 
