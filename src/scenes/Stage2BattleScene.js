513293fb94323b5771dac6d67b7e078e020832ef
wnstn4896 <wnstn4896@gmail.com>
2026-03-02 01:23:32 +0900

    modified:   src/scenes/Stage2BattleScene.js

--- src/scenes/Stage2BattleScene.js
+++ src/scenes/Stage2BattleScene.js
@@ -156,7 +156,7 @@ export class Stage2BattleScene extends Phaser.Scene {
 
         // 탄막 연사 이벤트
         this.time.addEvent({
-            delay: 100,
+            delay: 120,
             callback: this.shootPlayerBullet,
             callbackScope: this,
             loop: true,
