ddb5996073eb459765aef070b4f9fcfdaaf336f9
wnstn4896 <wnstn4896@gmail.com>
2026-03-09 17:10:05 +0900

    modified:   src/scenes/VideoCutScene.js

--- src/scenes/VideoCutScene.js
+++ src/scenes/VideoCutScene.js
@@ -2,6 +2,7 @@ export class VideoCutScene extends Phaser.Scene {
     constructor() {
         super({ key: 'VideoCutScene' });
 
+        this.stage;
     }
 
     init(data) {
@@ -12,11 +13,17 @@ export class VideoCutScene extends Phaser.Scene {
     create() {
         const video = this.add.video(640, 360, this.videoSrc);
 
+        switch (this.videoSrc){
+            case 'Nigerundayo':
+                this.stage = 2;
+                break;
+        }
+
         video.setMute(false);
         video.play();
 
         video.on('complete', () => {
-            this.scene.start('LoadingScene', { goToStage: 4 });
+            this.scene.start('LoadingScene', { goToStage: this.stage });
         });
     }
 }
